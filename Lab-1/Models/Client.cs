50dd9e8bb5845501272700ed77c918fc949dcef1
Adeola Adetoyese <adeolakautharadetoyese@mitt.ca>
2022-04-17 23:03:14 -0500

    Add project files.

---
+++ Lab-1/Models/Client.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab_1.Models
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(25,ErrorMessage ="First name must be between 3 and 25 characters", MinimumLength = 3)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(25, ErrorMessage = "Last name must be between 3 and 25 characters", MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "Phone number is required")]
+        [Display(Name = "Phone Number")]
+        [RegularExpression (@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Please enter a valid phone number")]
+        public int PhoneNumber { get; set; }
+
+    }
+}
---
+++ Lab-1/Models/Room.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab_1.Models
+{
+    public enum Section
+    {
+        First,
+        Second,
+        Third
+    }
+    public class Room
+    {
+        [Key]
+        [Display(Name ="Room Number")]
+        public int RoomNumber { get; set; }
+        [Display(Name = "Capacity")]
+        public int Capacity { get; set; }
+        public Section Section { get; set; }
+    }
+}
